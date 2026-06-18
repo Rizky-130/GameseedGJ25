eb6358ab6deea99b4685cebb596ca5bffed515f5
YeheyaBoy <airadzakyikhwan@gmail.com>
2026-06-18 20:25:04 +0700

    setting panel

--- Assets/Script/UI/MenuManager.cs
+++ Assets/Script/UI/MenuManager.cs
@@ -8,17 +8,21 @@ public class MenuManager : MonoBehaviour
 
     public RectTransform menuPanel;
     public RectTransform creditPanel;
+    public RectTransform settingsPanel;
 
     public Vector2 mainMenuCenter;
     public Vector2 mainMenuLeft;
     public Vector2 creditHiddenRight;
     public Vector2 creditCenter;
+    public Vector2 creditHiddenLeft;
 
     private bool transitioning = false;
 
     void Start()
     {
         creditPanel.anchoredPosition = creditHiddenRight;
+
+        settingsPanel.anchoredPosition = creditHiddenLeft;
     }
 
     public void PlayGame()
@@ -45,6 +49,18 @@ public class MenuManager : MonoBehaviour
     {
         StopAllCoroutines();
         StartCoroutine(CloseCreditsAnimation());
+    }
+
+    public void OpenSettings()
+    {
+        StopAllCoroutines();
+        StartCoroutine(OpenSettingsAnimation());
+    }
+
+    public void ReturnFromSettings()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CloseSettingsAnimation());
     }
 
     IEnumerator OpenCreditsAnimation()
@@ -137,4 +153,96 @@ public class MenuManager : MonoBehaviour
 
         transitioning = false;
     }
+
+    IEnumerator OpenSettingsAnimation()
+{
+    transitioning = true;
+
+    float duration = 1.0f;
+    float time = 0f;
+
+    Vector2 menuStart = menuPanel.anchoredPosition;
+    Vector2 menuTarget = new Vector2(2528, 0);
+
+    while (time < duration)
+    {
+        float t = Mathf.SmoothStep(0, 1, time / duration);
+
+        menuPanel.anchoredPosition =
+            Vector2.Lerp(menuStart, menuTarget, t);
+
+        time += Time.deltaTime;
+        yield return null;
+    }
+
+    menuPanel.anchoredPosition = menuTarget;
+
+    yield return new WaitForSeconds(0.1f);
+
+    time = 0f;
+
+    Vector2 settingsStart = settingsPanel.anchoredPosition;
+    Vector2 settingsTarget = new Vector2(2528, -100);
+
+    while (time < duration)
+    {
+        float t = Mathf.SmoothStep(0, 1, time / duration);
+
+        settingsPanel.anchoredPosition =
+            Vector2.Lerp(settingsStart, settingsTarget, t);
+
+        time += Time.deltaTime;
+        yield return null;
+    }
+
+    settingsPanel.anchoredPosition = settingsTarget;
+
+    transitioning = false;
+}
+
+IEnumerator CloseSettingsAnimation()
+{
+    transitioning = true;
+
+    float duration = 1.0f;
+    float time = 0f;
+
+    Vector2 settingsStart = settingsPanel.anchoredPosition;
+    Vector2 settingsTarget = new Vector2(0, 0);
+
+    while (time < duration)
+    {
+        float t = Mathf.SmoothStep(0, 1, time / duration);
+
+        settingsPanel.anchoredPosition =
+            Vector2.Lerp(settingsStart, settingsTarget, t);
+
+        time += Time.deltaTime;
+        yield return null;
+    }
+
+    settingsPanel.anchoredPosition = settingsTarget;
+
+    yield return new WaitForSeconds(0.1f);
+
+    time = 0f;
+
+    Vector2 menuStart = menuPanel.anchoredPosition;
+    Vector2 menuTarget = Vector2.zero;
+
+    while (time < duration)
+    {
+        float t = Mathf.SmoothStep(0, 1, time / duration);
+
+        menuPanel.anchoredPosition =
+            Vector2.Lerp(menuStart, menuTarget, t);
+
+        time += Time.deltaTime;
+        yield return null;
+    }
+
+    menuPanel.anchoredPosition = menuTarget;
+
+    transitioning = false;
+}
 }
